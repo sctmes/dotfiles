557be7f6b61d76432507752b45f90b0e5c247324
Yu Sun <ysun@sctmes.com>
2026-07-07 20:43:33 +0800

    fix: use bash for orca ssh targets

--- hosts/116/orca-remote.nix
+++ hosts/116/orca-remote.nix
@@ -1,4 +1,9 @@
-{ pkgs, ... }:
+{
+  lib,
+  pkgs,
+  username,
+  ...
+}:
 
 {
   environment.systemPackages = with pkgs; [
@@ -8,4 +13,13 @@
     gcc
     python3Minimal
   ];
+
+  users.users = {
+    # Workaround for Orca SSH relay bootstrap assuming POSIX login-shell
+    # semantics on the remote host. Nushell remains installed, but these Orca
+    # target users need bash until upstream handles non-POSIX login shells.
+    # https://github.com/stablyai/orca/issues/7715
+    ${username}.shell = lib.mkForce pkgs.bashInteractive;
+    zky.shell = lib.mkForce pkgs.bashInteractive;
+  };
 }
