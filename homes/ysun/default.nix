0d55528ee977da4d6654f87596ca8bb6f3a4f8e8
Yu Sun <ysun@sctmes.com>
2026-07-07 13:26:31 +0800

    fix: update ssh home-manager settings

--- homes/ysun/default.nix
+++ homes/ysun/default.nix
@@ -48,12 +48,12 @@ in
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks."github.com" = {
-      hostname = "github.com";
-      user = "git";
-      port = 22;
-      identityFile = "~/.ssh/id_ed25519_github";
-      identitiesOnly = true;
+    settings."github.com" = {
+      HostName = "github.com";
+      User = "git";
+      Port = 22;
+      IdentityFile = "~/.ssh/id_ed25519_github";
+      IdentitiesOnly = true;
     };
   };
 
